a81be3aec3cfed456dd347c4ed10833bc84a45a5
DianaCiceu <73532458+DianaCiceu@users.noreply.github.com>
2020-10-28 22:35:17 +0200

    Tema

---
+++ Ciceu_Diana-Maria/L5/Primitives.IO/Attributes/MockEffectAttribute.cs
@@ -0,0 +1,17 @@
+﻿  
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primitives.IO.Attributes
+{
+    public class MockEffectAttribute : Attribute
+    {
+        public object Effect { get; }
+
+        public MockEffectAttribute(object effect)
+        {
+            Effect = effect;
+        }
+    }
+}
